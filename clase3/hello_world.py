12bc70cc19db3f5f811234fc4560fa844ac749b6
ricardourvina <rurvinac@estudiante.uc.cl>
2025-03-07 11:02:23 -0300

    Clase4: Expresiones

---
+++ clase3/hello_world.py
@@ -0,0 +1,4 @@
+x = 0.6 
+y = x+3*0.3
+print(x)
+print(y)
---
+++ clases/Clase05_comparaciones_control_flujo/operadores_operandos.py
@@ -0,0 +1,22 @@
+print(20 + 32)
+#52
+hour=60
+minute=1
+hour - 1
+#59
+print(hour * 60 + minute)
+#3601
+print(minute / 60)
+#0.016666666666666666
+print(5 ** 2)
+#25
+
+
+grado=45
+pi=3.14159
+print(2 * (3-1))
+#4
+print((1+1)**(5-2))
+#8
+print(grado / 2 * pi)
+#70.68
