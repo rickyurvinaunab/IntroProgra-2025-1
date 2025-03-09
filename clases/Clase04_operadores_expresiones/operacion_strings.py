d56c892514e6d2960118ca71894e8cdf4f369182
ricardourvina <rurvinac@estudiante.uc.cl>
2025-03-09 17:56:40 -0300

    Clase 4

---
+++ clases/Clase04_operadores_expresiones/operacion_strings.py
@@ -0,0 +1,34 @@
+#Operador concatenación
+mensaje1 = 'Hola' + ' ' + 'Mundo'
+print(mensaje1)
+# Hola Mundo
+
+#Operador multiplicación
+mensaje2a = 'Hola ' * 3
+mensaje2b = 'Mundo'
+print(mensaje2a + mensaje2b)
+# Hola Hola Hola Mundo
+
+#Operador para añadir
+mensaje3 = 'Hola'
+mensaje3 += ' '
+mensaje3 += 'Mundo'
+print(mensaje3)
+#Hola Mundo
+
+#Sumar 1 a una variable
+numero1 = 2
+numero1 = numero1 + 1
+print(numero1)
+#3
+
+#Operador para añadir con variables numéricas
+numero1 = 2
+numero1 += 1
+print(numero1)
+#3
+
+#Extensión
+mensaje4 = 'hola' + ' ' + 'mundo'
+print(len(mensaje4))
+#10
---
+++ clases/Clase04_operadores_expresiones/transformaciones.py
@@ -0,0 +1,45 @@
+#Transformación de entero a flotante
+numero_entero = 10
+print(numero_entero)
+# 10
+numero_flotante = float(numero_entero)
+print(numero_flotante)
+# 10.0
+
+#Transformación de flotante a entero
+numero_flotante = 10.6
+print(numero_flotante)
+#10.6
+numero_entero = int(numero_flotante)
+print(numero_entero)
+#10
+
+#Redonde de números
+numero_flotante = 10.6
+print(numero_flotante)
+#10.6
+numero_entero = round(numero_flotante)
+print(numero_entero)
+#11
+
+
+#Transformación de entero a cadena
+numero_ejemplo = 4
+string_ejemplo = "Ejemplo número " + str(numero_ejemplo)
+print(string_ejemplo)
+
+
+#Transformación de flotante a string
+numero_flotante = 3.14159
+cadena_caracteres = str(numero_flotante)
+print(cadena_caracteres)
+
+#Transformación de cadena a entero
+cadena_caracteres = "123"
+numero_entero = int(cadena_caracteres)
+print(numero_entero)
+
+#Transformación de cadena a entero
+cadena_caracteres = "3.14"
+numero_flotante = float(cadena_caracteres)
+print(numero_flotante)
