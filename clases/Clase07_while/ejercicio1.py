9a5098f0f6f2913e21b96b2c933544805efb2b23
ricardourvina <rurvinac@estudiante.uc.cl>
2025-03-15 12:55:33 -0300

    Clase 07

---
+++ clases/Clase07_while/ejercicio1.py
@@ -0,0 +1,14 @@
+# Ejercicio 1: Cuenta Ascendente
+# Escribe un programa que utilice un bucle while para contar de 1 a 10 e imprima cada número.
+
+# Inicializar la variable contador
+contador = 1
+# Iniciar el bucle while
+while contador <= 10:
+    # Imprimir el número
+    print(contador)
+    # Incrementar el contador
+    contador += 1
+
+# Imprimir mensaje de fin
+print("Fin del programa")
---
+++ clases/Clase07_while/ejercicios_while.py
@@ -0,0 +1,26 @@
+# n = 5
+# while n > 0:
+#     print(n)
+#     n = n-1
+# print("!!terminado!!")
+
+n = 5
+while n > 0 :
+    print('Enjabonar')
+    print('Enjuagar')
+print('Secar')
+
+
+# n = 0
+# while n > 0 :
+#     print('Enjabonar')
+#     print('Enjuagar')
+# print('Secar')
+
+
+# while True:
+#     línea = input('> ')
+#     if línea == 'terminado':
+#         break
+#     print(línea)
+# print('terminado con éxito')
