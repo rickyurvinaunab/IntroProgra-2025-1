c454474cdacc4d8678300c999ce1b177bb289bbc
ricardourvina <rurvinac@estudiante.uc.cl>
2025-03-19 12:40:23 -0300

    Clase 09

---
+++ clases/Clase04_operadores_expresiones/clas04.py
@@ -0,0 +1,15 @@
+# distancia = 150
+distancia = input("Ingrese la distancia: ")
+distancia = int(distancia)
+consumo = float(input("Ingrese el consumo del vehiculo: "))
+precio_combustible = float(input("Ingrese el precio del combustible: "))
+costo_peajes = int(input("Ingrese el costo de los peajes: "))
+
+#calculo de consumo total de gasolina
+consumo_gasolina = distancia * consumo
+#calculo costo combustible
+costo_combustible = consumo_gasolina * precio_combustible
+#calcular costo total
+costo_total = costo_combustible + costo_peajes
+
+print(costo_total)
