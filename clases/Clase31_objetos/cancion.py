34875e06e9edce063022e7b38f846c50aa6d4a08
ricardourvina <rurvinac@estudiante.uc.cl>
2025-05-26 00:54:00 -0400

    Clase 31,32,33

---
+++ clases/Clase31_objetos/cancion.py
@@ -0,0 +1,20 @@
+class Cancion:
+    def __init__(self, titulo, artista):
+        # Aquí van los atributos
+
+    def reproducir(self):
+        # Aquí va el mensaje que simula la reproducción
+
+    def __str__(self):
+        # Aquí va la representación en texto de la canción
+
+
+# Crear canciones y probar los métodos
+c1 = Cancion("Shape of You", "Ed Sheeran")
+c2 = Cancion("Levitating", "Dua Lipa")
+
+print(c1)
+print(c1.reproducir())
+
+print(c2)
+print(c2.reproducir())
--- clases/Clase32_objetos2/clase_filmoteca.py
+++ clases/Clase32_objetos2/clase_filmoteca.py
@@ -32,36 +32,6 @@ class Filmoteca:
                 mejor = pelicula
         return mejor
 
-    def peliculas_por_categoria(self, categoria):
-        resultado = []
-        for pelicula in self.peliculas:
-            if categoria in pelicula.categoria:
-                resultado.append(pelicula)
-        return resultado
-
-    def obtener_promedios(self):
-        for pelicula in self.peliculas:
-            print(pelicula.nombre + " :", str(pelicula.promedio))
-
-    def peliculas_por_rango_anio(self, anio_inicio, anio_fin):
-        resultado = []
-        for pelicula in self.peliculas:
-            if anio_inicio <= pelicula.anio <= anio_fin:
-                resultado.append(pelicula)
-        return resultado
-
-    def contar_por_categoria(self):
-        conteo = []
-        for pelicula in self.peliculas:
-            encontrado = False
-            for item in conteo:
-                if item[0] == pelicula.categoria:
-                    item[1] += 1
-                    encontrado = True
-                    break
-            if not encontrado:
-                conteo.append([pelicula.categoria, 1])
-        return conteo
 
 
 pelicula1 = Pelicula("Una nueva esperanza", 1997, "Ciencia ficción", "Un joven granjero intercepta una llamada de socorro")
@@ -80,17 +50,6 @@ filmoteca = Filmoteca([pelicula1, pelicula2, pelicula3])
 print(filmoteca)
 print("Conteo por año:", filmoteca.contar_por_anio())
 print("Mejor pelicula:", filmoteca.mejor_pelicula())
-print("Peliculas de Ciencia ficcion:")
-for pelicula in filmoteca.peliculas_por_categoria("Ciencia ficcion"):
-    print(pelicula)
-
-print("Peliculas entre 2010 y 2015:")
-for pelicula in filmoteca.peliculas_por_rango_anio(2010, 2015):
-    print(pelicula)
-
-print("Conteo por categoria:", filmoteca.contar_por_categoria())
-
-filmoteca.obtener_promedios()
 
 print("¿Es 'Una nueva esperanza' mejor que 'La amenaza fantasma'?:", pelicula2.es_mejor_que(pelicula1))
 print("¿Es 'Dunkirk' mejor que 'La amenaza fantasma'?:", pelicula3.es_mejor_que(pelicula2))
