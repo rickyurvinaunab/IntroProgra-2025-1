cb9eea7e72e0072cc4fddc5cb663a572ee67d397
ricardourvina <rurvinac@estudiante.uc.cl>
2025-06-04 17:02:56 -0400

    Clase 34

--- clases/Clase32_objetos2/15320/filmoteca.py
+++ clases/Clase32_objetos2/15320/filmoteca.py
@@ -1,4 +1,8 @@
 from pelicula import Pelicula
+# __init__: Constructor que inicializa el atributo peliculas con una lista de instancias de Pelicula.
+# __str__: Retorna una cadena que indica el número total de películas en la filmoteca.
+# agregar_pelicula: Recibe una instancia de Pelicula y la agrega a la lista de peliculas.
+
 
 class Filmoteca:
 
@@ -12,30 +16,44 @@ class Filmoteca:
     def agregar_pelicula(self, pelicula):
         self.peliculas.append(pelicula)
     
-    def contar_despues_2000(self):
-        contador = 0
+# contar_por_anio: Cuenta cuántas películas hay en la filmoteca por año de lanzamiento y retorna una lista con el conteo.
+    def contar_por_anio(self):
+        lista_anios = []
         for peli in self.peliculas:
-            if peli.anio >= 2000:
-                contador += 1
+            if peli.anio not in lista_anios:
+                lista_anios.append(peli.anio)
+        indice_a = 0
+        for anio in lista_anios:
+            contador_anios = 0
+            for peli in self.peliculas:
+                if anio == peli.anio:
+                    contador_anios += 1
+                    lista_anios[indice_a] =[anio, contador_anios]
+            indice_a += 1
+        return lista_anios
+# mejor_pelicula: Encuentra y retorna la película con el mayor puntaje promedio.
+    
+    def mejor_pelicula(self):
+        lista_promedios = []
+        for peli in self.peliculas:
+            lista_promedios.append(peli.promedio)
         
-        return contador
-
+        return lista_promedios
+            
+        
 
 filmoteca = Filmoteca("Netflix")
 pelicula1 = Pelicula("Star Wars 1", 1999, "Ficcion","Una pelicula de ficcion")
 pelicula2 = Pelicula("Star Wars 2", 2002,"Ficcion","Otro pelicula de ficcion")
 pelicula3 = Pelicula("Star Wars 3", 2005, "Ficcion","Una pelicula de ficcion")
-pelicula4 = Pelicula("Star Wars 4", 2007, "Ficcion","Una pelicula de ficcion")
-pelicula5 = Pelicula("Star Wars 5", 2009, "Ficcion","Una pelicula de ficcion")
+pelicula4 = Pelicula("Star Wars 4", 2002, "Ficcion","Una pelicula de ficcion")
+pelicula5 = Pelicula("Star Wars 5", 1999, "Ficcion","Una pelicula de ficcion")
 filmoteca.agregar_pelicula(pelicula1)
 filmoteca.agregar_pelicula(pelicula2)
 filmoteca.agregar_pelicula(pelicula3)
 filmoteca.agregar_pelicula(pelicula4)
 filmoteca.agregar_pelicula(pelicula5)
 print(filmoteca)
-pelis_2000 = filmoteca.contar_despues_2000()
-print("La cantidad de peliculas desde el 2000 son:", pelis_2000)
-
 pelicula1.agregar_puntuacion(4.5)
 pelicula1.agregar_puntuacion(1)
 pelicula1.agregar_puntuacion(2)
@@ -46,3 +64,4 @@ pelicula3.agregar_puntuacion(5)
 pelicula3.agregar_puntuacion(3)
 pelicula3.agregar_puntuacion(1)
 
+print(filmoteca.contar_por_anio())
--- clases/Clase32_objetos2/15320/pelicula.py
+++ clases/Clase32_objetos2/15320/pelicula.py
@@ -12,4 +12,10 @@ class Pelicula:
     def agregar_puntuacion(self, puntuacion):
         self.puntos.append(puntuacion)
         self.promedio = sum(self.puntos) / len(self.puntos)
-   
+
+    def es_mejor_que(self, pelicula):
+        # Recibe otra instancia de Pelicula como parámetro y 
+        # retorna True si la película actual tiene un puntaje 
+        # promedio mayor que la otra, o False en caso contrario.
+        return self.promedio > pelicula.promedio
+
